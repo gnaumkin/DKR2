b697bcb21fed08573f47c380b51c807deb61d377
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-12-26 14:30:29 +0300

    dkr,z1.pas

--- dkr,z1.pas
+++ dkr,z1.pas
@@ -1,16 +1,16 @@
 ﻿program dkr2;
 var arr: array[1..10] of integer;
-sum,i: integer;
+sum,i: integer; // фактические, по ссылке, глобальные
 begin
   writeln('Введите элемент массива:');
-  for i:=1 to 10 do
+  for i:=1 to 10 do // локальные
     readln(arr[i]);
   sum:=0;
     for i:=1 to 10 do
     begin
     if i mod 2 <> 0 then
-      sum := sum + arr[i];
+      sum := sum + arr[i]; // формальные
   end;
-  writeln('Сумма элементов на нечетных позициях: ', sum);
+  writeln('Сумма элементов на нечетных позициях: ', sum); // фактические
   readln;
 end.
